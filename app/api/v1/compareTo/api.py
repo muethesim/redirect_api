e553a146559f7e427d30e49a0213b550a36d6143
muethesim <muethesim@gmail.com>
2025-08-19 22:30:50 +0530

    Initial

--- app/api/v1/compareTo/api.py
+++ app/api/v1/compareTo/api.py
@@ -1,4 +1,42 @@
 from fastapi import APIRouter
-
+from fastapi.responses import JSONResponse
+import requests
+from app.utils.constants import constants
+from app.core.config import settings
+from app.api.v1.compareTo.schemas import HotelInlineRequest
 
 router = APIRouter(prefix="/compareTo", tags=["Compare To"])
+
+
+@router.get("/hotel-list")
+def get_hotel_list(payload: HotelInlineRequest):
+    url = constants.BASE_URL_COMPARETO + constants.HOTEL_LIST_ENDPOINT
+    params = {"apiKey": settings.COMPARETO_ADS_API_KEY, "userTrackId": payload.userTrackId}
+
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+        "x-original-client-ip": payload.clientIP,
+    }
+
+    payload = {
+        "showOn": payload.showOn,
+        "checkinDate": payload.checkinDate,
+        "checkoutDate": payload.checkoutDate,
+        "adults": payload.adults,
+        "rooms": payload.rooms,
+        "cityId": payload.cityId,
+    }
+    response = requests.post(
+        url,
+        params=params,
+        headers=headers,
+        json=payload,
+        cookies=payload.cookies if payload.cookies else {},
+    )
+    response_data = {
+        "status_code": response.status_code,
+        "response": response.json(),
+        "cookies": dict(response.cookies),
+    }
+    return JSONResponse(content=response_data)
